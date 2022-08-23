c286adfa94b3d11879196f1751d8d3aa10e1d7f0
Jaime <jaimeuribe585@gmail.com>
2022-08-23 16:49:18 -0500

    cambio plantillas

--- Fit/urls.py
+++ Fit/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('home',views.home, name='inicio'),
+    path('',views.home, name='inicio'),
     path('calorias', views.calorias , name='calorias'),
     path('usuario', views.usuario , name='usuario'),
     path('crearusuario', views.crearusuario , name='crearusuario'),
