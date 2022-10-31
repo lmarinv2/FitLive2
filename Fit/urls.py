36a7e5e680813035130da35d638095fbd4832415
lauramarin452 <lauramarin452@gmail.com>
2022-10-31 17:32:02 -0500

    inicio estadistica bebidas

--- Fit/urls.py
+++ Fit/urls.py
@@ -9,15 +9,17 @@ urlpatterns = [
    #path('registration/login', views.iniciar_sesion,name='login'),
     path('perfil/', views.usuario , name='usuario'),
     path('Seleccionar_meta/',views.Seleccionar_meta,name='Seleccionar_meta'),
-    path('ejercicio/', views.actividad , name='actividad'),
-    path('actividad_fisica/', views.actividad_fisica , name='actividad_fisica'),
+    path('ejercicio', views.actividad , name='actividad'),
+    #path('actividad_fisica', views.actividad_fisica , name='actividad_fisica'),
     path('calendario',views.calendario,name='calendario'),
     path('index',views.index,name='index'),
     path('registro_comidas',views.registro_comidas,name='registro_comidas'),
-    path('agregar_bebidas/<int:email>',views.agregar_bebidas,name='agregar_bebidas'),
-    path('registro_bebidas',views.registro_bebidas,name='registro_bebidas'),
+    path('agregar_bebidas',views.agregar_bebidas,name='agregar_bebidas'),
+    #path('registro_bebidas',views.registro_bebidas,name='registro_bebidas'),
     path('alimentacion',views.alimentacion,name='alimentacion'),
     path('agregar_comida',views.agregar_comida,name='agregar_comida'),
+    path('estadisticas',views.estadisticas,name='estadisticas'),
+
     
     
     
--- Fit/views.py
+++ Fit/views.py
@@ -12,6 +12,8 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from datetime import date, datetime
 from datetime import datetime
+import datetime
+import pandas as pd
 
 emailperfil = ""
 idperfil = ""
@@ -19,6 +21,9 @@ idperfil = ""
 #from django.contrib.auth.tokens import
 # Create your views here.
 
+#def iniciar_sesion(request):
+#    return render(request, 'registration/login.html')
+#################################################################################################################################
 def home(request):
     email = request.POST.get("email")
     global emailperfil
@@ -36,10 +41,10 @@ def home(request):
                 idperfil = sexo[a][0]
         a = a+1;
     return render(request,'home.html')
-
+###########################################################################################################################
 def index(request):
     return render(request,'index.html')
-
+##########################################################################################################################
 def crearusuario(request):
     formulario = registroForm(request.POST or None)
     if formulario.is_valid():
@@ -47,15 +52,12 @@ def crearusuario(request):
         messages.success(request,"Registado correctamente")
         return redirect('registros')
     return render(request, 'crearusuario.html',{'formulario':formulario})
-
-#def iniciar_sesion(request):
-#    return render(request, 'registration/login.html')
-
+###########################################################################################################################
 def usuario(request,):
     direcciones=registro.objects.filter(Email__icontains=emailperfil)
     calorias()
     return render(request, "usuario.html",{"registros":direcciones})
-
+###########################################################################################################################
 def calorias():
     sexo = registro.objects.values_list('Genero','Edad','Peso','Email')
     sexo = [list(elem) for elem in sexo]
@@ -63,7 +65,6 @@ def calorias():
     edad = [list(elem) for elem in edad]
     peso = registro.objects.values_list('Peso')
     peso = [list(elem) for elem in peso]
-
     a = 0;
     for i in peso:         
         b = 0;
@@ -105,13 +106,8 @@ def calorias():
                     caloria = 11.711 * sexo[a][2]  + 587.7;
                     registro.objects.filter(Email=sexo[a][3]).update(calorias=caloria)
         a = a+1;
-
-def actividad_fisica(request):
-
-    return render(request, "actividad_fisica1.html")
-
-
-def actividad(request,):
+###########################################################################################################################
+def actividad(request):
     print(request.POST.get("Fecha"))
     if request.method == "POST":
             dep = request.POST.get("Deporte")
@@ -239,89 +235,99 @@ def actividad(request,):
                         b.save()
             else:
                 pass
-            messages.success(request, "Deporte añadido")
+            messages.success(request, "Deporte añadido correctamente")
 
     return render(request, "actividad_fisica.html")
+###########################################################################################################################
+def agregar_bebidas(request):
+    if request.method == "POST":
+            beb = request.POST.get("Bebida")
+            cant = request.POST.get("Cantidad")
+            fech = request.POST.get("Fecha")
+            if beb == "Agua":
+                        b = Bedidas(usuario=registro(pk=idperfil), Bebida=beb, Cantidad= cant, calorias_Bedida = 0, Fecha =fech)
+                        b.save()
+            elif beb == "Jugo":
+                if cant == "250":
+                    b = Bedidas(usuario=registro(pk=idperfil), Bebida=beb, Cantidad= cant, calorias_Bedida = 137, Fecha =fech )
+                    b.save()
+                elif cant == "500":
+                    b = Bedidas(usuario=registro(pk=idperfil), Bebida=beb, Cantidad= cant, calorias_Bedida = 275, Fecha =fech )
+                    b.save()
+                elif cant == "1000":
+                    b = Bedidas(usuario=registro(pk=idperfil), Bebida=beb, Cantidad= cant, calorias_Bedida = 550, Fecha =fech )
+                    b.save()
+                   
+            elif beb == "Gaseosa":
+                if cant == "250":
+                    b = Bedidas(usuario=registro(pk=idperfil), Bebida=beb, Cantidad= cant, calorias_Bedida = 102, Fecha =fech )
+                    b.save()
+                elif cant == "500":
+                    b = Bedidas(usuario=registro(pk=idperfil), Bebida=beb, Cantidad= cant, calorias_Bedida = 205, Fecha =fech )
+                    b.save()
+                elif cant == "1000":
+                    b = Bedidas(usuario=registro(pk=idperfil), Bebida=beb, Cantidad= cant, calorias_Bedida = 410, Fecha =fech )
+                    b.save()
 
+            elif beb == "Infusion":
+                if cant == "250":
+                    b = Bedidas(usuario=registro(pk=idperfil), Bebida=beb, Cantidad= cant, calorias_Bedida = 2, Fecha =fech )
+                    b.save()
+                elif cant == "500":
+                    b = Bedidas(usuario=registro(pk=idperfil), Bebida=beb, Cantidad= cant, calorias_Bedida = 4, Fecha =fech )
+                    b.save()
+                elif cant == "1000":
+                    b = Bedidas(usuario=registro(pk=idperfil), Bebida=beb, Cantidad= cant, calorias_Bedida = 6, Fecha =fech )
+                    b.save()
+            else:
+                pass
+            messages.success(request, "Bebida añadida")
+    return render(request,'agregar_bebidas.html')
+###########################################################################################################################
 def Seleccionar_meta(request):
-    
     if request.method=='POST':
         print(request)
         if request.POST.get('Seleccionar_meta'):
             savemeta=Metas()
             savemeta.eleccion=request.POST.get('Seleccionar_meta')
             savemeta.save()
-            messages.success(request, "Meta seleccionada correctamente")
-            
-            
+            messages.success(request, "Meta seleccionada correctamente")       
     return render(request,"Seleccionar_meta.html")
-
+###########################################################################################################################
 def alimentacion(request):
     return render(request,'alimentacion.html')
-
-def registro_bebidas(request):
-    return render(request,'registro_bebidas.html')    
-
-def agregar_bebidas(request,email):
-    if request.method == "POST":
-            beb = request.POST.get("Bebida")
-            cant = request.POST.get("Cantidad")
-            fech = request.POST.get("Fecha")
-            print(request.POST.get("Fecha"))
-            if beb == "Agua":
-                        b = Bedidas(usuario=registro(pk=email), Bedida=beb, Cantidad= cant, calorias_Bedida = 0, Fecha =fech)
-                        b.save()
-            elif beb == "Jugo":
-                if cant == "250":
-                    b = Bedidas(usuario=registro(pk=email), Bebida=beb, Cantidad= cant, calorias_Bedida = 137, Fecha =fech )
-                    b.save()
-                elif cant == "500":
-                    b = Bedidas(usuario=registro(pk=email), Bebida=beb, Cantidad= cant, calorias_Bedida = 275, Fecha =fech )
-                    b.save()
-                elif cant == "1000":
-                    b = Bedidas(usuario=registro(pk=email), Bebida=beb, Cantidad= cant, calorias_Bedida = 550, Fecha =fech )
-                    b.save()
-                   
-            elif beb == "Gaseosa":
-                if cant == "250":
-                    b = Bedidas(usuario=registro(pk=email), Bebida=beb, Cantidad= cant, calorias_Bedida = 102, Fecha =fech )
-                    b.save()
-                elif cant == "500":
-                    b = Bedidas(usuario=registro(pk=email), Bebida=beb, Cantidad= cant, calorias_Bedida = 205, Fecha =fech )
-                    b.save()
-                elif cant == "1000":
-                    b = Bedidas(usuario=registro(pk=email), Bebida=beb, Cantidad= cant, calorias_Bedida = 410, Fecha =fech )
-                    b.save()
-
-            elif beb == "Infusion":
-                if cant == "250":
-                    b = Bedidas(usuario=registro(pk=email), Bebida=beb, Cantidad= cant, calorias_Bedida = 2, Fecha =fech )
-                    b.save()
-                elif cant == "500":
-                    b = Bedidas(usuario=registro(pk=email), Bebida=beb, Cantidad= cant, calorias_Bedida = 4, Fecha =fech )
-                    b.save()
-                elif cant == "1000":
-                    b = Bedidas(usuario=registro(pk=email), Bebida=beb, Cantidad= cant, calorias_Bedida = 6, Fecha =fech )
-                    b.save()
-            else:
-                pass
-            messages.success(request, "Bebida añadida")
-    return render(request,'agregar_bebidas.html')
-
+###########################################################################################################################
 def calendario(request):
     return render(request,'calendario.html')
-
-
-
+###########################################################################################################################
+def estadisticas(request):
+    if request.method == "POST":
+        fechinc = request.POST.get("Fechainicio")
+        fechfin = request.POST.get("Fechafinal")
+        start = datetime.datetime.strptime(fechinc, "%Y-%m-%d")
+        end = datetime.datetime.strptime(fechfin, "%Y-%m-%d")
+        date_generated = pd.date_range(start, end)
+        usuario = registro.objects.filter(pk=idperfil).values_list('pk')
+        usuario = [list(elem) for elem in usuario]
+        beb = Bedidas.objects.values_list('Bebida','Fecha','calorias_Bedida','usuario')
+        beb = [list(elem) for elem in beb]
+        print(beb)
+        a = 0;
+        for i in date_generated:         
+            b = 0;
+            if date_generated[a] == emailperfil:
+                b = b+1;
+            a = a+1;
+        
+    return render(request,'estadisticas.html')
+###########################################################################################################################
 def registro_comidas(request):
     return render(request,'registro_comidas.html')
     print(datetime.today())
-
+###########################################################################################################################
 def agregar_comida(request):
-   
     return render(request,'agregar_comida.html')
-
-
+###########################################################################################################################
 def agregar_comida(request):
     if request.method=='POST':
         form=comidaForm(request.POST)
@@ -332,6 +338,7 @@ def agregar_comida(request):
         form=comidaForm()
         
     return render(request,'agregar_comida.html',{'form':form})
+###########################################################################################################################
 
     
 
